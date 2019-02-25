ea2c4bead98af4ec6e7e87b817c983b9c97ae3ed
Icytroll <Jens_soendervindgab@hotmail.com>
2019-02-25 23:24:51 +0100

    Need to include analytic logistic function

--- Programming/exercise_orbit/main.c
+++ Programming/exercise_orbit/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 double mylogistic(double x);
 double myequatorial(double x, double eps, double y0, double yp0);
 
@@ -17,7 +18,7 @@ int main()
 	double y0[3]  = {1,1,1,};
 	double yp0[3] = {0,0.5,0.5};
 
-	for(double phi=0;phi<=12;phi+=0.05)
+	for(double phi=0;phi<=10*2*M_PI;phi+=0.05)
 		fprintf(stderr,"%g %g %g %g\n",phi,myequatorial(phi,eps[0],y0[0],yp0[0])
 										  ,myequatorial(phi,eps[1],y0[1],yp0[1])
 										  ,myequatorial(phi,eps[2],y0[2],yp0[2]));

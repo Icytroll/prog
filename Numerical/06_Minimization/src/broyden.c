0d3197858c7b49c93065ec742d5ebd597671f86b
Icytroll <Jens_soendervindgab@hotmail.com>
2019-04-09 18:09:52 +0200

    A done

---
+++ Numerical/06_Minimization/src/broyden.c
@@ -0,0 +1,53 @@
+#include"matrix.h"
+#include"vector.h"
+#include<math.h>
+
+void qr_gs_decomp(matrix* Q, matrix* R);
+void qr_gs_solve(matrix* Q, matrix* R, vector* b, vector* x);
+
+void broyden(
+	double f(vector* x),
+	void set_df(vector* x, vector* df),
+	vector* x,
+	double epsilon) {
+	
+	int n = x->size,maxiter = 1000,iter;
+	double alpha = 1e-4, lambda, f_x, f_trial;
+	vector* df = vector_alloc(n);
+	matrix* H = matrix_alloc(n,n);
+	matrix* R = matrix_alloc(n,n);
+	vector* Dx = vector_alloc(n);
+	vector* x_trial = vector_alloc(n);
+
+	vector_set_all(Dx,1);
+	
+	set_dfH(x,df,H);
+	iter = 0;
+	while(norm(df) > epsilon && iter<maxiter) {
+		qr_gs_decomp(H,R);
+		vector_mult_all(df,-1);
+		qr_gs_solve(H,R,df,Dx);
+		vector_mult_all(df,-1);
+		
+		lambda = 1;
+		f_x = f(x);
+		vector_add(x,Dx,x_trial);
+		f_trial = f(x_trial);
+		while(f_trial > f_x+alpha*vector_inner(Dx,df) && lambda > 1.0/64) {
+			lambda /= 2;
+			vector_mult_all(Dx,0.5);
+			vector_add(x,Dx,x_trial);
+			f_trial = f(x_trial);
+		}
+		vector_add(x,Dx,x);
+		set_dfH(x,df,H);
+		iter++;
+	}
+	fprintf(stderr,"%10d\n",iter);
+
+	vector_free(df);
+	matrix_free(H);
+	matrix_free(R);
+	vector_free(Dx);
+	vector_free(x_trial);
+}
---
+++ Numerical/06_Minimization/src/main.c
@@ -0,0 +1,171 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<float.h>
+#include<gsl/gsl_matrix.h>
+#include<gsl/gsl_vector.h>
+#include"matrix.h"
+#include"vector.h"
+
+/*--- FUNCTION DECLARATIONS ---*/
+
+// TEST-FUNCTIONS AND THEIR DERIVATIVES/HESSIAN
+
+// Rosenbrocks Valley
+double f1 (vector* x) {
+	double x1 = vector_get(x,0);
+	double x2 = vector_get(x,1);
+	return (1-x1)*(1-x1) + 100*(x2-x1*x1)*(x2-x1*x1);
+}
+
+void set_dfH1 (vector* x, vector* df, matrix* H) {
+	double x1 = vector_get(x,0);
+	double x2 = vector_get(x,1);
+	vector_set(df,0,2*x1 - 400*x1*(-(x1*x1)+x2) - 2);
+	vector_set(df,1,-200*x1*x1 + 200*x2);
+	matrix_set(H,0,0,1200*x1*x1 - 400*x2 + 2);
+	matrix_set(H,0,1,-400*x1);
+	matrix_set(H,1,0,-400*x1);
+	matrix_set(H,1,1,200);
+}
+
+// Himmelblau Function
+double f2 (vector* x) {
+	double x1 = vector_get(x,0);
+	double x2 = vector_get(x,1);
+	return pow(x1*x1+x2-11,2) + pow(x1+x2*x2-7,2);
+}
+
+void set_dfH2 (vector* x, vector* df, matrix* H) {
+	double x1 = vector_get(x,0);
+	double x2 = vector_get(x,1);
+	vector_set(df,0,2*x1 + 4*x1*(x1*x1+x2-11) + 2*x2*x2 - 14);
+	vector_set(df,1,2*x2 + 4*x2*(x2*x2+x1-7) + 2*x1*x1 - 22);
+	matrix_set(H,0,0,12*x1*x1 + 4*x2 - 42);
+	matrix_set(H,0,1,4*x1 + 4*x2);
+	matrix_set(H,1,0,4*x1 + 4*x2);
+	matrix_set(H,1,1,12*x2*x2 + 4*x1 - 26);
+}
+
+// MINIMIZATION FUNCTIONS
+
+void newton(
+	double f(vector* x),
+	void set_dfH(vector* x, vector* df, matrix* H),
+	vector* x,
+	double epsilon);
+
+/*--- MAIN PROGRAM ---*/
+
+int main() {
+	
+	fprintf(stderr,"ITERATIONS:\n\n");
+	fprintf(stderr,"(Rosenbrock Valley)\n");
+	fprintf(stderr,"(Himmelblau function)\n");
+	
+	// A - Newton's method with analytic gradient and hessian, and back-tracking linesearch
+	
+	fprintf(stderr,"\nNewton with analytic gradient and hessian:\n");
+	
+	int n = 2;
+	vector* x = vector_alloc(n);
+	double epsilon = 1e-10;
+	
+	printf("Minimization using analytical gradient and hessian:\n\n");
+	vector_set(x,0,3);
+	vector_set(x,1,6);
+	
+	vector_set(x,0,2);
+	vector_set(x,1,1);
+	printf("Minimum of the Rosenbrock valley:\n");
+	vector_print(x,"x0 =",stdout);
+	newton(f1,set_dfH1,x,epsilon);
+	vector_print(x,"x_final =",stdout);
+	fprintf(stdout,"f(x_final) = %g\n",f1(x));
+		
+	vector_set(x,0,5);
+	vector_set(x,1,5);
+	printf("1 of the minimum of the Himmelblau function:\n");
+	vector_print(x,"x0 =",stdout);
+	newton(f2,set_dfH2,x,epsilon);
+	vector_print(x,"x_final =",stdout);
+	fprintf(stdout,"f(x_final) = %g\n",f2(x));
+	
+	// B - Newton's method with numerical Jacobian and back-tracking linesearch
+	// Also testing GSL root multiroot finder
+	/*
+	fprintf(stderr,"\nNewton with numerical jacobian:\n");
+	
+	double dx = sqrt(DBL_EPSILON);
+	gsl_vector* x_gsl = gsl_vector_alloc(n);
+
+	printf("Root finding using numerical jacobian\n\n");
+	
+	vector_set(x,0,3);
+	vector_set(x,1,6);
+	printf("1 of the solutions to the first system of equations:\n");
+	vector_print(x,"x0 =",stdout);
+	newton(f1,x,dx,epsilon);
+	vector_print(x,"x_final =",stdout);
+	f1(x,fx);
+	vector_print(fx,"f(x_final) =",stdout);
+	
+	
+	vector_set(x,0,2);
+	vector_set(x,1,1);
+	vector_print(x,"x0 =",stdout);
+	printf("Minimum of the Rosenbrock valley:\n");
+	newton(f2,x,dx,epsilon);
+	vector_print(x,"x_final =",stdout);
+	f2(x,fx);
+	vector_print(fx,"f(x_final) =",stdout);
+	
+	
+	vector_set(x,0,5);
+	vector_set(x,1,5);
+	printf("1 of the minimum of the Himmelblau function:\n");
+	vector_print(x,"x0 =",stdout);
+	newton(f3,x,dx,epsilon);
+	vector_print(x,"x_final =",stdout);
+	f3(x,fx);
+	vector_print(fx,"f(x_final) =",stdout);
+	
+	// C - Newton's method with refined linesearch
+	
+	fprintf(stderr,"\nNewton with refined linesearch:\n");
+	
+	printf("Root finding using refined linesearch\n\n");
+	
+	vector_set(x,0,3);
+	vector_set(x,1,6);
+	printf("1 of the solutions to the first system of equations:\n");
+	vector_print(x,"x0 =",stdout);
+	newton_quadline(f1,df1,x,epsilon);
+	vector_print(x,"x_final =",stdout);
+	f1(x,fx);
+	vector_print(fx,"f(x_final) =",stdout);
+	
+	vector_set(x,0,2);
+	vector_set(x,1,1);
+	vector_print(x,"x0 =",stdout);
+	printf("Minimum of the Rosenbrock valley:\n");
+	newton_quadline(f2,df2,x,epsilon);
+	vector_print(x,"x_final =",stdout);
+	f2(x,fx);
+	vector_print(fx,"f(x_final) =",stdout);
+	
+	vector_set(x,0,5);
+	vector_set(x,1,5);
+	printf("1 of the minimum of the Himmelblau function:\n");
+	vector_print(x,"x0 =",stdout);
+	newton_quadline(f3,df3,x,epsilon);
+	vector_print(x,"x_final =",stdout);
+	f3(x,fx);
+	vector_print(fx,"f(x_final) =",stdout);
+	*/
+	// Clean up
+
+	vector_free(x);
+	
+	return 0;
+}

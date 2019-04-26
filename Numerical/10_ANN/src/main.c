d14c041f96ad9402085b26e5ad5ab67ddc1a0efb
Icytroll <Jens_soendervindgab@hotmail.com>
2019-04-26 15:02:40 +0200

    Created folders

---
+++ Numerical/10_ANN/src/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<float.h>
+#include<math.h>
+#include<unistd.h>
+#include<assert.h>
+#include"vector.h"
+#include"matrix.h"
+
+/*--- FUNCTION DECLARATIONS ---*/
+
+
+/*--- MAIN PROGRAM ---*/
+
+int main() {
+	
+	// A - Train network to minimze errors for 1D data
+	
+	
+	// B - Same but for 2D data
+	
+		
+	// C - ??
+	
+	
+	return 0;
+}
